cd66f5fdf4e72851d2e7f73fc11ca97909b816ee
Ellis Rhys Thomas <e.rhys.thomas@gmail.com>
2021-07-01 18:51:14 +0300

    renamed pnid_bbox to pnid_box

---
+++ src/pnid_box.c
@@ -0,0 +1,193 @@
+/* This file is part of pnid
+   Copyright (C) 2021 Ellis Rhys Thomas <e.rhys.thomas@gmail.com>
+   See COPYING file for licence details */
+
+/* pnid_box.c - pnid bounding box, a rectangle */
+
+#include <assert.h>
+
+#include "pnid_box.h"
+
+static unsigned umin(unsigned x, unsigned y);
+static unsigned umax(unsigned x, unsigned y);
+
+/* pnid_box_get_left(): return left side of rectangle a */
+unsigned
+pnid_box_get_left(const struct pnid_box *a)
+{
+    return a->nw.x;
+}
+
+/* pnid_box_get_left(): set left side of rectangle a */
+void
+pnid_box_set_left(struct pnid_box *a, unsigned left)
+{
+    a->nw.x = left;
+}
+
+/* pnid_box_get_right(): return right edge of rectangle a */
+unsigned
+pnid_box_get_right(const struct pnid_box *a)
+{
+    return a->se.x;
+}
+
+/* pnid_box_set_right(): return right edge of rectangle a */
+void
+pnid_box_set_right(struct pnid_box *a, unsigned right)
+{
+    a->se.x = right;
+}
+
+/* pnid_box_get_top(): top edge of rectangle a */
+unsigned
+pnid_box_get_top(const struct pnid_box *a)
+{
+    return a->nw.y;
+}
+
+/* pnid_box_set_top(): top edge of rectangle a */
+void
+pnid_box_set_top(struct pnid_box *a, unsigned top)
+{
+    a->nw.y = top;
+}
+
+/* pnid_box_get_bottom(): return bottom edge of rectangle a */
+unsigned
+pnid_box_get_bottom(const struct pnid_box *a)
+{
+    return a->se.y;
+}
+
+/* pnid_box_set_bottom(): set bottom edge of rectangle a */
+void
+pnid_box_set_bottom(struct pnid_box *a, unsigned bottom)
+{
+    a->se.y = bottom;
+}
+
+/* pnid_box_height(): return vertical extent of rectangle a */
+unsigned
+pnid_box_height(const struct pnid_box *a)
+{
+    return pnid_box_get_bottom(a) - pnid_box_get_top(a); 
+}
+
+/* pnid_box_copy(): returns a copy of the rectangle */
+struct pnid_box
+pnid_box_copy(const struct pnid_box *a)
+{
+  return *a;
+}
+
+/* pnid_box_width(): horizontal extent of rectangle b */
+unsigned
+pnid_box_width(const struct pnid_box *a)
+{
+    return pnid_box_get_left(a) - pnid_box_get_right(a);
+}
+
+/* pnid_box_perimeter(): length of the perimiter */
+unsigned
+pnid_box_perimeter(const struct pnid_box *a)
+{
+    return 2 * (pnid_box_width(a) + pnid_box_height(a));
+}
+
+/* pnid_box_area(): area of rectangle a */
+unsigned
+pnid_box_area(const struct pnid_box *a)
+{
+    return pnid_box_width(a) * pnid_box_height(a);
+}
+
+/* pnid_box_is_separate(): true only when rectangles share no common
+   areas. */
+int
+pnid_box_is_separate(const struct pnid_box *a, const struct pnid_box *b)
+{
+    return
+	pnid_box_get_left(a)   >= pnid_box_get_right(b)  &&
+	pnid_box_get_right(a)  <= pnid_box_get_left(b)   &&
+	pnid_box_get_top(a)    >= pnid_box_get_bottom(b) &&
+	pnid_box_get_bottom(a) <= pnid_box_get_top(b);
+}
+
+/* pnid_box_mbr(): minimum bounding rectangle of a and b. */
+struct pnid_box
+pnid_box_mbr(const struct pnid_box *a, const struct pnid_box *b)
+{
+    struct pnid_box ab;
+
+    ab.nw.x = umin(a->nw.x, b->nw.x); /* left */
+    ab.se.x = umax(a->se.x, b->se.x); /* right */
+    ab.nw.y = umin(a->nw.y, b->nw.y); /* top */
+    ab.se.y = umax(a->se.y, b->se.y); /* bottom */
+
+    return ab;
+}
+
+/* pnid_box_enlargement(): area increase in mbr when it is grown to
+   include a. */
+unsigned
+pnid_box_mbr_enlargement(const PnidBox *mbr, const PnidBox *a)
+{
+    const struct pnid_box ab = pnid_box_mbr(mbr, a);
+
+    return pnid_box_area(&ab) - pnid_box_area(a);
+}
+
+/* pnid_box_mbr_waste(): returns the wastefulness of a minimum
+   bounding rectangle containing rectangles a and b.
+
+   The wastefulness is calculated as excess in mbr area of above that
+   of the individual areas of a and b. Concequently the wastefulness
+   can be negative when the rectangles overlap. */
+int
+pnid_box_mbr_waste(const struct pnid_box *a, const struct pnid_box *b)
+{
+  const struct pnid_box mbr = pnid_box_mbr(a, b);
+
+  return pnid_box_area(&mbr) - pnid_box_area(a) - pnid_box_area(b);
+}
+
+/* pnid_box_mbr_grow(): update mbr so that it can contain a. */
+void
+pnid_box_mbr_grow(PnidBox *mbr, const PnidBox *a)
+{
+  *mbr = pnid_box_mbr(mbr, a);
+}
+
+/* pnid_box_overlap_area(): area of overlap between rectangles a and b */
+unsigned
+pnid_box_overlap_area(const struct pnid_box *a, const struct pnid_box *b)
+{
+    struct pnid_box ab;
+
+    if (pnid_box_is_separate(a, b))
+	return 0;
+
+    ab.nw.x = umax(a->nw.x, b->nw.x); /* left */
+    ab.se.x = umin(a->se.x, b->se.x); /* right */
+    ab.nw.y = umax(a->nw.y, b->nw.y); /* top */
+    ab.se.y = umin(a->se.y, b->se.y); /* bottom */
+
+    return pnid_box_area(&ab);
+}
+
+/* umin(): returns the smallest unsigned integer */
+static unsigned
+umin(unsigned x, unsigned y)
+{
+    return x < y ? x : y;
+}
+
+/* umin(): returns the largest unsigned integer */
+static unsigned
+umax(unsigned x, unsigned y)
+{
+    return x > y ? x : y; 
+}
+    
+
---
+++ src/pnid_box.h
@@ -0,0 +1,52 @@
+/* This file is part of pnid
+   Copyright (C) 2021 Ellis Rhys Thomas <e.rhys.thomas@gmail.com>
+   See COPYING file for licence details */
+
+/* pnid_box.h - pnid boundary boxes */
+
+#ifndef __PNID_BOX_H
+#define __PNID_BOX_H
+
+#include <stdarg.h>
+
+struct pnid_coord {
+    unsigned x;
+    unsigned y;
+};
+
+struct pnid_box {
+    struct pnid_coord nw;
+    struct pnid_coord se;
+};
+
+typedef struct pnid_coord PnidCoord;
+typedef struct pnid_box   PnidBox;
+
+PnidBox pnid_box_copy(const struct pnid_box *a);
+
+/* Get properties of the rectangle a */
+unsigned pnid_box_get_left   (const PnidBox *a);
+unsigned pnid_box_get_right  (const PnidBox *a);
+unsigned pnid_box_get_top    (const PnidBox *a);
+unsigned pnid_box_get_bottom (const PnidBox *a);
+
+/* Calculations on a rectangle */
+unsigned pnid_box_height    (const PnidBox *a);
+unsigned pnid_box_width     (const PnidBox *a);
+unsigned pnid_box_perimeter (const PnidBox *a);
+unsigned pnid_box_area      (const PnidBox *a);  
+
+/* Calculations on two rectangles */
+unsigned pnid_box_overlap_area    (const PnidBox *a,   const PnidBox *b);
+PnidBox  pnid_box_mbr             (const PnidBox *a,   const PnidBox *b);
+unsigned pnid_box_mbr_enlargement (const PnidBox *mbr, const PnidBox *a);
+int      pnid_box_mbr_waste       (const PnidBox *mbr, const PnidBox *a);
+void     pnid_box_mbr_grow        (PnidBox *mbr,       const PnidBox *a);
+
+/* Set basic properties of a rectangle a */
+void pnid_box_set_left   (PnidBox *a, unsigned left);
+void pnid_box_set_right  (PnidBox *a, unsigned right);
+void pnid_box_set_top    (PnidBox *a, unsigned top);
+void pnid_box_set_bottom (PnidBox *a, unsigned bottom);
+
+#endif /* PNID_H */
